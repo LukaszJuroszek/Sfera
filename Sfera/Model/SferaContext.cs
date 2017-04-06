975171a3fa7b5e36b16668204fc29af059fae432
Łukasz Juroszek <lukjuroszek@gmail.com>
2017-04-06 16:48:37 +0200

    add dbsets

--- Sfera/Model/SferaContext.cs
+++ Sfera/Model/SferaContext.cs
@@ -4,6 +4,14 @@ namespace Sfera.Model
 {
     public class SferaContext :DbContext
     {
+        public DbSet<Obiekt> Obiekty { get; set; }
+        public DbSet<Korytarz> Korytarze { get; set; }
+        public DbSet<ObiektDoWynajecia> ObiektyDoWynajecia { get; set; }
+        public DbSet<Parking> Parkingi { get; set; }
+        public DbSet<Pomieszczenie> Pomieszczenia { get; set; }
+        public DbSet<PomieszczenieTechniczne> PomieszczeniaTechniczne { get; set; }
+        public DbSet<Poziom> Poziomy { get; set; }
+        public DbSet<Stand> Standy { get; set; }
 
     }
 }
